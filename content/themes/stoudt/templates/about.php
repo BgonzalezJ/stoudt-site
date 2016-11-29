fed784b7ed3b0bf6e94140380b7fc78a3e5e4302
Bastián Gonzalez <bastian.gonzalez@wikot.com>
2016-11-29 16:32:58 -0300

    UPDATE: Ajustes de diseño y arreglos de errores

--- content/themes/stoudt/templates/about.php
+++ content/themes/stoudt/templates/about.php
@@ -21,17 +21,17 @@
 
 ?>
 
-<style>
-	#about .text * {
-		color: <?= $txtColor; ?>!important;
-	}
-
-	#about .text .btn.contact-email {
-		border-color: <?= $txtColor; ?>!important;
-	}
-</style>
-
 <?php get_header(); ?>
+
+	<style>
+		#about .text * {
+			color: <?= $txtColor; ?>!important;
+		}
+
+		#about .text .btn.contact-email {
+			border-color: <?= $txtColor; ?>!important;
+		}
+	</style>
 
 	<script>
 		jQuery(function($) {
@@ -56,28 +56,33 @@
 		});
 	</script>
 
-	<div style="overflow: hidden; background: <?= $background ?>;">
-		<div class="container" id="about">
-			
-			<div class="row">
-				
 
-				<div class="col-md-6 col-sm-6 col-lg-6 col-xs-12 text pull-right">
-					<div>
-						<h2><?= get_the_subtitle(get_the_ID()); ?></h2>
-						<?= get_the_content(); ?>
-						<a href="mailto:<?= $email ?>" class="btn contact-email"><?= $email ?></a>
+
+	
+	<?php while (have_posts()): the_post(); ?>
+		<div style="overflow: hidden; background: <?= $background ?>;">
+			<div class="container" id="about">
+				<div class="row">
+					<div class="col-md-6 col-sm-6 col-lg-6 col-xs-12 text pull-right">
+						<div>
+							<h2><?= get_the_subtitle(get_the_ID()); ?></h2>
+							<?php the_content(); ?>
+
+							<p>Get in touch:</p>
+
+							<a href="mailto:<?= $email ?>" class="btn contact-email"><?= $email ?></a>
+						</div>
+
+					</div>
+
+					<div class="col-md-6 col-sm-6 col-lg-6 col-xs-12 img text-center pull-left">
+						<img src="<?= wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[0]; ?>" class="img-side" style="max-width: <?= wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[1] ?>px; " />
 					</div>
 
 				</div>
 
-				<div class="col-md-6 col-sm-6 col-lg-6 col-xs-12 img text-center pull-left">
-					<img src="<?= wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[0]; ?>" class="img-side" style="max-width: <?= wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[1] ?>px; " />
-				</div>
-
 			</div>
-
 		</div>
-	</div>
+	<?php endwhile; ?>
 
 <?php get_footer(); ?>
